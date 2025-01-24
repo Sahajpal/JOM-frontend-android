4a0e4774495b5228ef09ab2415f92a0eb73f2c61
Sahajpal <54036040+Sahajpal@users.noreply.github.com>
2025-01-24 15:29:11 +0530

    "refactor: Refactored the project structure :hammer:"

--- app/src/main/java/com/liranoir/jom/MainActivity.kt
+++ app/src/main/java/com/liranoir/jom/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             JOMTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "JOM!",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Let commence $name!",
         modifier = modifier
     )
 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     JOMTheme {
-        Greeting("Android")
+        Greeting("JOM")
     }
 }
